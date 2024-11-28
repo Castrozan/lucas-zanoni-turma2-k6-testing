e9bc0a4955220912daa2a1f1c797a0b02c502051
Lucas De Castro Zanoni <lucas.69412@alunosatc.edu.br>
2024-11-28 20:11:43 -0300

    feat: prova

--- src/tests/Tests.spec.js
+++ src/tests/Tests.spec.js
@@ -1,16 +1,27 @@
 import { htmlReport } from 'https://raw.githubusercontent.com/benc-uk/k6-reporter/main/dist/bundle.js';
 import http from 'k6/http';
 import { check, sleep } from 'k6';
-import { Trend } from 'k6/metrics';
+import { Rate, Trend } from 'k6/metrics';
 
 export const getComputersDuration = new Trend('get_contacts', true);
+export const requestsSuccessRate = new Rate('SUCCESS_RATE');
 
 export const options = {
   thresholds: {
-    http_req_failed: ['rate<0.01'],
-    http_req_duration: ['avg<10000']
+    http_req_failed: ['rate<0.12'],
+    http_req_duration: ['p(95)<5700']
   },
-  stages: [{ duration: '1m', target: 1000 }]
+  stages: [
+    { duration: '25s', target: 10 },
+    { duration: '20s', target: 50 },
+    { duration: '55s', target: 50 },
+    { duration: '20s', target: 100 },
+    { duration: '55s', target: 100 },
+    { duration: '20s', target: 180 },
+    { duration: '80s', target: 180 },
+    { duration: '10s', target: 300 },
+    { duration: '15s', target: 300 }
+  ]
 };
 
 export function handleSummary(data) {
@@ -34,6 +45,7 @@ export default function () {
   const res = http.get(`${baseUrl}`, params);
 
   getComputersDuration.add(res.timings.duration);
+  requestsSuccessRate.add(res.status === OK);
 
   check(res, {
     'get computers - status 200': () => res.status === OK
